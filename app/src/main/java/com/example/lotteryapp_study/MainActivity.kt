cb2179ccc46e84376e708f522e3d301088a6e508
eunjin2118 <ally6324@naver.com>
2022-07-25 11:10:28 +0900

    first commit

--- app/src/main/java/com/example/lotteryapp_study/MainActivity.kt
+++ app/src/main/java/com/example/lotteryapp_study/MainActivity.kt
@@ -14,7 +14,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val pref = getSharedPreferences("nums", Context.MODE_PRIVATE)
 //        val rnds = (1..45).random()
 //        val nums= mutableListOf<Int>()
